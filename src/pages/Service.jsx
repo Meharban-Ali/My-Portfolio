a099a2a6e0cf55ddb7a7b74296b87198c1a53b2d
Meharban-Ali <meharbanali7900@gmail.com>
2025-03-11 23:18:46 +0530

    changes

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ServicesSection } from '../components/Service/ServicesSection'
-import { Offer_Service } from '../components/Service/offer_service'
+import { Offer_Service } from '../components/Service/Offer_Service'
 import { Type_Of_Service } from '../components/Service/Type_Of_Service'
 import { Contact_form } from '../components/Project/Contact_form'
 
